e2c82f9e12e0ed835346a6ac777da703d1282e39
Anton Dolgopolik <antondolgopolikv@gmail.com>
2021-09-15 15:07:08 +0300

    Add lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(5);
+            for (int i = 0; i < 10; i++)
+            {
+                int j = i;
+                taskQueue.EnqueueTask(delegate
+                {
+                    for (int k = j * 10; k < (j + 1) * 10; k++)
+                    {
+                        Console.Out.WriteLine("k = {0}", k);
+                        Thread.Sleep(1000);
+                    }
+                });
+            }
+
+            taskQueue.Stop();
+        }
+    }
+}
---
+++ Lab1/TaskQueue.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Lab1
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+
+        private readonly Queue<TaskDelegate> _tasks = new();
+        private Thread[] _threadPool;
+        private volatile bool isStopped;
+
+        public TaskQueue(int threadPoolSize)
+        {
+            _threadPool = new Thread[threadPoolSize];
+            for (int i = 0; i < threadPoolSize; i++)
+            {
+                _threadPool[i] = new Thread(Run);
+                _threadPool[i].Start();
+            }
+        }
+
+        private void Run()
+        {
+            TaskDelegate task = null;
+            var tasksArePresent = true;
+            // Execute tasks
+            while (!isStopped || tasksArePresent)
+            {
+                lock (_tasks)
+                {
+                    tasksArePresent = _tasks.Count != 0;
+                    if (tasksArePresent)
+                    {
+                        task = _tasks.Dequeue();
+                    }
+                }
+
+                if (task != null)
+                {
+                    task();
+                    task = null;
+                }
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (_tasks)
+            {
+                _tasks.Enqueue(task);
+            }
+        }
+
+        public void Stop()
+        {
+            isStopped = true;
+        }
+    }
+}
